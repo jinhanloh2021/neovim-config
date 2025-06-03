3c30f8c4fe00868962f7ea6ce6364ac5e5e97cac
jinhanloh2021 <jinhan.loh.2021@scis.smu.edu.sg>
2025-06-03 09:46:20 +0800

    Add go support

--- lua/jinhan/plugins/lsp/lspconfig.lua
+++ lua/jinhan/plugins/lsp/lspconfig.lua
@@ -5,16 +5,13 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
+		-- mason.nvim and mason-lspconfig.nvim are configured in mason.lua,
+		-- but good to list them as dependencies here too for load order.
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 	},
 	config = function()
-		-- import lspconfig plugin
 		local lspconfig = require("lspconfig")
-
-		-- import mason_lspconfig plugin
-		local mason_lspconfig = require("mason-lspconfig")
-
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		local keymap = vim.keymap -- for conciseness
 
@@ -48,7 +45,7 @@ return {
 				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
 				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show diagnostics for file
 
 				opts.desc = "Show line diagnostics"
 				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
@@ -67,48 +64,11 @@ return {
 			end,
 		})
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
-
 		-- Change the Diagnostic symbols in the sign column (gutter)
-		-- (not in youtube nvim video)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		local signs = { Error = "• ", Warn = "• ", Hint = "• ", Info = "• " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
-
-		mason_lspconfig.setup_handlers({
-			-- default handler for installed servers
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["graphql"] = function()
-				-- configure graphql language server
-				lspconfig["graphql"].setup({
-					capabilities = capabilities,
-					filetypes = { "graphql", "gql", "typescriptreact", "javascriptreact" },
-				})
-			end,
-			["lua_ls"] = function()
-				-- configure lua server (with special settings)
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							-- make the language server recognize "vim" global
-							diagnostics = {
-								globals = { "vim" },
-							},
-							completion = {
-								callSnippet = "Replace",
-							},
-						},
-					},
-				})
-			end,
-		})
 	end,
 }
--- lua/jinhan/plugins/lsp/mason.lua
+++ lua/jinhan/plugins/lsp/mason.lua
@@ -3,15 +3,17 @@ return {
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
-		-- import mason
 		local mason = require("mason")
-
-		-- import mason-lspconfig
 		local mason_lspconfig = require("mason-lspconfig")
-
 		local mason_tool_installer = require("mason-tool-installer")
+
+		-- Import cmp_nvim_lsp for capabilities
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
 		-- enable mason and configure icons
 		mason.setup({
@@ -24,7 +26,7 @@ return {
 			},
 		})
 
-		-- LSP
+		-- LSP: Configure mason-lspconfig with the new 'handlers' API
 		mason_lspconfig.setup({
 			-- list of servers for mason to install
 			ensure_installed = {
@@ -43,6 +45,39 @@ return {
 				"sqlls",
 				"tailwindcss",
 				"yamlls",
+			},
+			automatic_installation = true,
+			handlers = {
+				-- Default handler for all servers installed by Mason-LSPconfig
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+						-- Add common on_attach functions here if you have them,
+						-- or define them in your lspconfig.lua
+						-- on_attach = on_attach,
+					})
+				end,
+				["graphql"] = function()
+					require("lspconfig")["graphql"].setup({
+						capabilities = capabilities,
+						filetypes = { "graphql", "gql", "typescriptreact", "javascriptreact" },
+					})
+				end,
+				["lua_ls"] = function()
+					require("lspconfig")["lua_ls"].setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+								completion = {
+									callSnippet = "Replace",
+								},
+							},
+						},
+					})
+				end,
 			},
 		})
 
